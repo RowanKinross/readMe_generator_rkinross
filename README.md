daa1946e95b39df4b38028ce8f921ebe1b5a6e2c
RowanKinross <rowankinross@yahoo.co.uk>
2024-02-02 18:33:31 +0000

    readme_for_repo_added_pakage_lock_and_node_modules_added

---
+++ README.md
@@ -0,0 +1,30 @@
+#Web APIs Code Quiz - Flight of the Conchords Trivia
+
+## Project status
+Development for this project is on pause, for now.
+
+## Description
+
+https://
+
+## Visuals
+The following animation shows the appearance, usage and user interface of the application:
+
+![demo]
+
+## Installation
+n/a
+
+## Usage
+
+
+## Contributing
+As this is a personal project, designed to challenge and test my skills, I am not currently looking for contributors. However, there may be a opening for this in the future.
+
+## Credits
+
+edX challenge materials for setting the challenge and providing me with the skills to take on this project.
+
+## License
+
+MIT License - refer to LICENSE in the repo.
--- index.js
+++ index.js
@@ -3,38 +3,82 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
-//Title (#) 
-  // ask for title
 
-// Sections:
-  //( if any left blank, put "n/a" )
+inquirer
+  .prompt([
+    {
+  //Title (#) 
+    // ask for title
+      type: 'input',
+      message: 'What is the title of the project?',
+      name: 'title',
+    },
 
   //## Description
-    // repo name
     // [Project name] is an [application, project, etc] that...
     // The [application, project, etc] takes...
     // Users can...
     // The project makes use of...(tools, languages, strategies)
+    {
+      type: 'input',
+      message: 'What is the name of the project?',
+      name: 'projectName',
+    },
+    {
+      type: 'rawlist',
+      message: 'Which of these best describes the project:',
+      name: 'projectType',
+      choices: ['project', 'application', 'web-application', 'tool', 'game', 'product'],
+      default: [0]
+    },
+    {
+      type: 'input',
+      message: 'Complete this sentence to explain the outcome of the project: The projects takes...',
+      name: 'projectTakes'
+    }
+  ])
+  //response is just a parameter, you could use 'banana' instead
+
+.then((response) => {
+const fs = require('fs');
+const fileName = `${response.name}.txt`
+
+fs.appendFile(fileName, 
+  `Name: ${response.name}\n 
+  Languages:${response.languages}\n 
+  Preferred Communication:${response.preferredLanguage}\n`, 
+(err) =>
+  err ? console.error(err) : console.log('data logged')
+  );
+ }
+);
+// array of questions for user
+const questions = [
+
+
+
   
   //## Installations
     // How do you install the application?
+    //default n/a
 
   //## Usage
     // Use a series of prompts i.e "Upon opening the (what best describes the project - project, application, page, repository, terminal), the user is "
 
   //## Demo
     //link to screencapture or gif of demo
+    //default n/a
 
   //## License (list options - allow to select one only)
     //find from github
 
   //## Contributors
     // github? yes or no > github name
+    //default n/a
 
   //## Tests
     // Short description on testing
+    //default n/a
 
   //## Questions
     //Enter github username
@@ -48,12 +92,13 @@ function writeToFile(fileName, data) {
 //Title (#) 
 
 // Sections:
-  //## Description
+// if any left blank, put n/a
+//## Description
     // [Project name] is an [application, project, etc] that...
+      //Capitalise project name
     // The [application, project, etc] takes...
     // Users can...
     // The project makes use of...(tools, languages, strategies)
-    //link to repo - https://github.com/${username}/${repo}
   
   //## Table of contents:
     //installation, usage, demo, license, contributing, tests, questions (installation instructions, usage information, contribution guidelines, and test instructions)
